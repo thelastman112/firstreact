f8bbf0820dc2d4efb8cb7d7788e68829b675f915
thelastman112 <rizkymaestro12@gmail.com>
2022-12-07 12:44:22 +0700

    add navbar#2

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import { NavLink } from "react-router-dom";
 
-const Navbar = () => {
-  return (
-    <nav className="flex bg-gray-300 justify-between">
-        <h1 className='px-10 py-5'>Navbar</h1>
-        <ul className='flex'>
-            <li><NavLink to={"#"}>What's New</NavLink></li>
-            <li><NavLink to={"#"}>About me</NavLink></li>
-        </ul>
-    </nav>
+const Navbar = () => {  
+    return (
+        <nav className="flex bg-gray-300 justify-between px-8">
+            <h1 className=' py-5'>Navbar</h1>
+            <ul className='flex py-5'>
+                <li className=''><NavLink to={"/Login"}>What's New</NavLink></li>
+                <li className='px-3'><NavLink to={"/Login"}>About me</NavLink></li>
+                <li className=''><NavLink to={"/Login"}>Profile</NavLink></li>
+            </ul>
+        </nav>
 
-  )
+    )
 }
 
 export default Navbar
